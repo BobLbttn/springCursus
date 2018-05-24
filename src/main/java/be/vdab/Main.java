7ed4ab14903a4d6de534f610de2b4a54d924e1b5
bob.lamsens <bob.lamsens@OntCur-0E18035F.vdabheverlee.intra>
2018-05-24 16:01:18 +0200

    einde klas 24/05 - dependency injection

--- src/main/java/be/vdab/Main.java
+++ src/main/java/be/vdab/Main.java
@@ -8,18 +8,23 @@ public class Main {
 		try (ClassPathXmlApplicationContext context =
 				new ClassPathXmlApplicationContext("container.xml")) { 
 		
-			ClassA objectA = context.getBean("classA", ClassA.class);
-			System.out.println(objectA.getTelefoonNrHelpDesk());
-			System.out.println(objectA.getKoersenURL());
-			System.out.println(objectA.getDefaultTaalEnLand().getDisplayName());
-			System.out.println("Bestand bestaat: " + objectA.getImportData().exists());
-			System.out.println(objectA.getDefaultBladRichting());
-			System.out.println(objectA.getWebMasterEMailAdres());
-			System.out.println(objectA.getWebsiteGestart());
-			ClassB objectB = context.getBean(ClassB.class);
-			System.out.println(objectB.getTelefoonNrHelpDesk());
-			System.out.println(objectB.getAantalPogingenUpdateKlant());
-			System.out.println(context.getBean(HelpDeskMedewerkers.class));
+//			ClassA objectA = context.getBean("classA", ClassA.class);
+//			System.out.println(objectA.getTelefoonNrHelpDesk());
+//			System.out.println(objectA.getKoersenURL());
+//			System.out.println(objectA.getDefaultTaalEnLand().getDisplayName());
+//			System.out.println("Bestand bestaat: " + objectA.getImportData().exists());
+//			System.out.println(objectA.getDefaultBladRichting());
+//			System.out.println(objectA.getWebMasterEMailAdres());
+//			System.out.println(objectA.getWebsiteGestart());
+//			ClassB objectB = context.getBean(ClassB.class);
+//			System.out.println(objectB.getTelefoonNrHelpDesk());
+//			System.out.println(objectB.getAantalPogingenUpdateKlant());
+//			System.out.println(context.getBean(HelpDeskMedewerkers.class));
+			context.getBean("teller1", Teller.class).verhoog(); 
+			context.getBean("teller1", Teller.class).verhoog(); 
+			Teller teller2 = context.getBean("teller2", Teller.class); 
+			teller2.verhoog(); 
+			context.getBean("teller2", Teller.class).verhoog();
 		}
 	}
 
