fdf7434e7cda02bc49c112a4987ce5b487f46710
bobl <git@bttn.be>
2018-05-28 15:10:06 +0200

    punt 8 : spring annontiations

--- src/main/java/be/vdab/Main.java
+++ src/main/java/be/vdab/Main.java
@@ -2,33 +2,18 @@ package be.vdab;
 
 import java.math.BigDecimal;
 
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import be.vdab.restclients.RestClientsConfig;
 import be.vdab.services.EuroService;
+import be.vdab.services.ServicesConfig;
 
 public class Main {
 
 	public static void main(String[] args) {
-		try (ClassPathXmlApplicationContext context =
-				new ClassPathXmlApplicationContext("restclients.xml", "services.xml")) { 
+		try (AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(
+				new Class[] {ServicesConfig.class, RestClientsConfig.class})) { 
 		
-//			ClassA objectA = context.getBean("classA", ClassA.class);
-//			System.out.println(objectA.getTelefoonNrHelpDesk());
-//			System.out.println(objectA.getKoersenURL());
-//			System.out.println(objectA.getDefaultTaalEnLand().getDisplayName());
-//			System.out.println("Bestand bestaat: " + objectA.getImportData().exists());
-//			System.out.println(objectA.getDefaultBladRichting());
-//			System.out.println(objectA.getWebMasterEMailAdres());
-//			System.out.println(objectA.getWebsiteGestart());
-//			ClassB objectB = context.getBean(ClassB.class);
-//			System.out.println(objectB.getTelefoonNrHelpDesk());
-//			System.out.println(objectB.getAantalPogingenUpdateKlant());
-//			System.out.println(context.getBean(HelpDeskMedewerkers.class));
-//			context.getBean("teller1", Teller.class).verhoog(); 
-//			context.getBean("teller1", Teller.class).verhoog(); 
-//			Teller teller2 = context.getBean("teller2", Teller.class); 
-//			teller2.verhoog(); 
-//			context.getBean("teller2", Teller.class).verhoog();
 			System.out.println(
 					context.getBean(EuroService.class).naarDollar(BigDecimal.valueOf(2)));
 		}
