13b3973c7156c99beef9b80f618df453f5463288
bobl <git@bttn.be>
2018-05-31 08:42:46 +0200

    einde 29/05/2018

--- src/main/java/be/vdab/services/ServicesConfig.java
+++ src/main/java/be/vdab/services/ServicesConfig.java
@@ -1,10 +1,15 @@
 package be.vdab.services;
 
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
-@ComponentScan
-public class ServicesConfig {
+import be.vdab.restclients.KoersenClient;
 
+@Configuration
+public class ServicesConfig {
+	@Bean
+	EuroService euroService(@Qualifier("ECB") KoersenClient koersenClient) { 
+		return new EuroService(koersenClient);
+	}
 }
